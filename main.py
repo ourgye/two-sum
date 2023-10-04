c5bb385f1de4fb244ad0ef1df750fdc123ee1b1b
hayoung <cadmium4488@gmail.com>
2023-10-04 10:21:05 +0000

    added self instance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def twoSum( nums: List[int], target: int) -> List[int]:
+def twoSum(self, nums: List[int], target: int) -> List[int]:
     size = len(nums)
     for i in range(size):
         for j in range(i+1, size):
